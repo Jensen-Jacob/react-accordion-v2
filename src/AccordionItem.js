6bf7d85c1c63e4573309d7c49ff060ba905b39c6
Jensen-Jacob <jensenjacobgeorge@gmail.com>
2023-09-22 16:57:51 +0530

    Refactor code to include handleToggle function and add an animation to the open class to make the green border top ease in

--- src/AccordionItem.js
+++ src/AccordionItem.js
@@ -4,11 +4,12 @@ import "./index.css";
 export default function AccordionItem({ num, title, text }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  function handleToggle() {
+    setIsOpen((open) => !open);
+  }
+
   return (
-    <div
-      className={`item ${isOpen ? "open" : ""}`}
-      onClick={() => setIsOpen((open) => !open)}
-    >
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
